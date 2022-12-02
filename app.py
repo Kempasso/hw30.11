46e6bc25c25018410c23102a231a744043e334e3
Anatoly <topplang1337@mail.ru>
2022-12-02 18:00:03 +0300

    f

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
+import re
+
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 
 class Controller:
-    @classmethod
-    def map(cls, data, parameter):
+    @staticmethod
+    def map(data, parameter: str) -> list[str]:
         return list(map(lambda x: x.split()[int(parameter)].replace('\"', ""), data))
 
-    @classmethod
-    def filter(cls, data, parameter):
+    @staticmethod
+    def filter(data, parameter: str) -> list[str]:
         return list(filter(lambda string: parameter in string, data))
 
-    @classmethod
-    def unique(cls, data, parameter):
+    @staticmethod
+    def unique(data, parameter: str) -> list[str]:
         return list(set(data))
 
-    @classmethod
-    def sort(cls, data, parameter):
+    @staticmethod
+    def sort(data, parameter: str) -> list[str]:
         return sorted(data, reverse=True if parameter == 'desk' else False)
 
-    @classmethod
-    def limit(cls, data, parameter):
+    @staticmethod
+    def limit(data, parameter: int) -> list[str]:
         return data[:int(parameter)]
 
+    @staticmethod
+    def regex(data, parameter: str) -> list[str]:
+        pattern = re.compile(f'{parameter}')
+        return [i for i in data if re.match(pattern, i)]
+#_io.TextIOWrapper
 
 @app.route('/perform_query', methods=['POST'])
 def foo():
-    data = request.get_json()
+    data = dict(request.form)
     filename = data['filename']
     with open(f'data/{filename}') as file:
         file_data = file
+        print(type(file_data))
         first_result = getattr(Controller, data['cmd1'])(file_data, data["value1"])
         if not data.get('cmd2', None):
             return jsonify(first_result)
@@ -38,4 +46,5 @@ def foo():
         return jsonify(result2)
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
